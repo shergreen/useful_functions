8d6c5fe528cb8dbe714238688530298c5fddb69b
Sheridan Beckwith Green <sheridan.green@yale.edu>
2018-07-27 21:25:39 -0400

    added plot

--- plotter.py
+++ plotter.py
@@ -14,3 +14,11 @@ def loglogplot(figsize=(7,5.8)):
     ax.xaxis.set_ticks_position('both')
     ax.tick_params(axis='both', which='minor', colors='black', width=1.0, length=3.0)
     return fig, ax
+
+def plot(figsize=(7,5.8)): #figure out what the standard figure size is for journal articles...
+    fig = plt.figure(figsize=figsize)
+    ax = fig.add_subplot(111)
+    ax.yaxis.set_ticks_position('both')
+    ax.xaxis.set_ticks_position('both')
+    ax.tick_params(axis='both', which='minor', colors='black', width=1.0, length=3.0)
+    return fig, ax
